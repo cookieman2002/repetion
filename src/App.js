7ef1ba72b3caa2e41686e2f4bd38bf038cb9eec9
Cookieman2002 <mcl2002@outlook.dk>
2023-03-10 13:36:53 +0100

    tried 2 fetch at the same time

--- src/App.js
+++ src/App.js
@@ -8,13 +8,29 @@ const [data, setData] = useState();
 
 useEffect(() => {
   (async function(){
-    const res = await axios.get("https://fitness-verden-api.onrender.com/api/v1/classes")
-    setData(res.data)
+    try {
+      
+      const res = await axios.get("https://fitness-verden-api.onrender.com/api/v1/classes")
+      const data = res.data
+      setData(data)
+      const trainers = data.map(item => item.trainer.id)
+      
+      const promises = trainers.map(async trainerId => {
+        const response = await axios.get(`https://fitness-verden-api.onrender.com/api/v1/trainers/${trainerId}`)
+        console.log(response.data)
+      })
+      
+      await Promise.all(promises)
+    } catch (error) {
+      console.log(error) 
+    }
+    
+  
   }())
 
 }, []);
 
-
+console.log(data)
 
 // async function confirmDelete(id){
 // if(showModal === "flex" && deleteId){
@@ -45,14 +61,14 @@ useEffect(() => {
       </div>
     </form> </div> : null} */}
 
-      {data ? data.map(item => (
-       <li  key={item.id}> 
-        <p className="text-xl font-extrabold" >{item.className}</p>
-        <p>{item.trainer.trainerName} - {new Date(item.createdAt).toLocaleDateString()}</p>
+      {/* {data ? data.map(item => ( */}
+      {/* //  <li  key={item.id}>  */}
+        {/* <p className="text-xl font-extrabold" >{item.className}</p> */}
+        {/* <p>{item.trainer.trainerName} - {new Date(item.createdAt).toLocaleDateString()}</p> */}
         {/* <button className="border bg-red-500 ml-5 border-blue-400" onClick={() => {confirmDelete(); setDeleteId(item.id)}}>Slet</button> */}
       
     
-      </li>)) : null}
+      {/* </li>)) : null} */}
       {/* <div className="gap-5 flex" style={{display: showModal}}>
         <button className="bg-red-600" onClick={() => confirmDelete()}>ja</button>
         <button className="bg-green-700" onClick={() => setShowModal("none")}>nej</button>
